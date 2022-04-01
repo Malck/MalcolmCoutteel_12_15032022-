a2f0a56abadfcaa2e23c37e77816de18ec229643
MC <Malckira@gmail.com>
2022-04-01 16:41:02 +0200

    fix data

--- Backend/app/data.js
+++ Backend/app/data.js
@@ -38,37 +38,37 @@ const USER_ACTIVITY = [
             {
                 day: '2020-07-01',
                 kilogram: 80,
-                calories: 40
+                calories: 240
             },
             {
                 day: '2020-07-02',
                 kilogram: 80,
-                calories: 70
+                calories: 220
             },
             {
                 day: '2020-07-03',
                 kilogram: 81,
-                calories: 80
+                calories: 280
             },
             {
                 day: '2020-07-04',
                 kilogram: 81,
-                calories: 90
+                calories: 290
             },
             {
                 day: '2020-07-05',
                 kilogram: 80,
-                calories: 60
+                calories: 160
             },
             {
                 day: '2020-07-06',
                 kilogram: 78,
-                calories: 62
+                calories: 162
             },
             {
                 day: '2020-07-07',
                 kilogram: 76,
-                calories: 90
+                calories: 390
             }
         ]
     },
@@ -78,37 +78,37 @@ const USER_ACTIVITY = [
             {
                 day: '2020-07-01',
                 kilogram: 70,
-                calories: 40
+                calories: 240
             },
             {
                 day: '2020-07-02',
                 kilogram: 69,
-                calories: 50
+                calories: 220
             },
             {
                 day: '2020-07-03',
                 kilogram: 70,
-                calories: 80
+                calories: 280
             },
             {
                 day: '2020-07-04',
                 kilogram: 70,
-                calories: 50
+                calories: 500
             },
             {
                 day: '2020-07-05',
                 kilogram: 69,
-                calories: 60
+                calories: 160
             },
             {
                 day: '2020-07-06',
                 kilogram: 69,
-                calories: 62
+                calories: 162
             },
             {
                 day: '2020-07-07',
                 kilogram: 69,
-                calories: 90
+                calories: 390
             }
         ]
     }
